f8f5dc665ebfe0831ce838f4c82cb09daaff1101
LL <li.lin@huairuo.ai>
2019-11-19 11:25:39 +0800

    add docx

---
+++ my_py_toolkit/file/doc/doc_toolkit.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Huairuo.ai.
+# Author: Lin Li (li.lin@huairuo.ai)
+#
+# cython: language_level=3
+#
+
+from docx import Document
+
+def get_paaragraphs(doc_path):
+  """
+  Gets the text of paragraphs.
+  Args:
+    doc_path(str): The file path.
+
+  Returns:
+    (list(str)): The text of paragraphs.
+  """
+  doc = Document(doc_path)
+  paras = []
+  for para in doc.paragraphs:
+    paras.append(para.text)
+  return paras
