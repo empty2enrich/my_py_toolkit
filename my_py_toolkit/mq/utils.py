65abc0344e1fe0bc15567a0b70a0076024c6a42a
LL <li.lin@huairuo.ai>
2020-02-27 10:55:15 +0800

    a part of mq.

--- my_py_toolkit/mq/utils.py
+++ my_py_toolkit/mq/utils.py
@@ -22,7 +22,7 @@ def exc_curl(command, content_type="json"):
   text = res.read()
   res.close()
   data = re.split("\n*|\r*", text)[-1]
-  if content_type == "json":
+  if content_type == "json" and data:
     data = json.loads(data)
   return data
 
@@ -78,3 +78,50 @@ def purge_queues(url, user, pwd, vhost, queue_name):
   Returns:
 
   """
+
+def create_vhost(url, user, pwd, vhost):
+  command = f'curl -i -u {user}:{pwd} -H "content-type:application/json" -XPUT {url}/api/vhosts/{vhost}'
+  res = exc_curl(command)
+  print(res)
+
+def query_consumer(url, user, pwd, v_host):
+  """"""
+  command = f"curl -i -u {user}:{pwd} {url}/api/consumers/{v_host}"
+  res = exc_curl(command)
+  print(res)
+
+
+def delete_queue(queues, ip, port, vhost, user, pwd):
+  import pika
+  conn = pika.BlockingConnection(pika.ConnectionParameters(host=ip,
+                                                           virtual_host=vhost,
+                                                           port=port,
+                                                           credentials=pika.PlainCredentials(user, pwd),
+                                                           heartbeat=0))
+  ch = conn.channel()
+
+  for q in queues:
+    ch.queue_delete(queue=q)
+  ch.close()
+
+
+def add_queue_or_purge(queues, ip, port, vhost, user, pwd, use_priority=False):
+  import pika
+
+  conn = pika.BlockingConnection(pika.ConnectionParameters(host=ip,
+                                                           virtual_host=vhost,
+                                                           port=port,
+                                                           credentials=pika.PlainCredentials(user, pwd),
+                                                           heartbeat=0))
+  ch = conn.channel()
+  for q in queues:
+    if use_priority:
+      ch.queue_declare(queue=q, durable=True, arguments={'x-max-priority': 10})
+    else:
+      ch.queue_declare(queue=q, durable=True)
+    ch.queue_purge(queue=q)
+  ch.close()
+
+if __name__=="__main__":
+  # create_vhost("", "*", "", "TEST_VHOST")
+  query_consumer("", "*", "", "")
