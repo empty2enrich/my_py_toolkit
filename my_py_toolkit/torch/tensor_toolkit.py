be145077b572fa67c514f3304f84947dabe6be83
emptyknowledge <2056374813@qq.com>
2020-04-16 11:34:03 +0800

    add

--- my_py_toolkit/torch/tensor_toolkit.py
+++ my_py_toolkit/torch/tensor_toolkit.py
@@ -5,6 +5,8 @@
 # cython: language_level=3
 #
 
+import math
+import torch
 from ..basic_data_type.basic_data_type_toolkit import *
 
 def mask(tensor, tensor_mask, mask_dim):
@@ -14,6 +16,7 @@ def mask(tensor, tensor_mask, mask_dim):
     tensor(torch.Tensor): 输入
     tensor_mask(torch.Tensor): mask 位置信息.
     mask_dim(int): 负数，指定需要 mask 的维度，example：mask_dim = -1, 表示在最后一维上做 mask 操作.
+      example: tensor is shape(3,3,3), -1 表示对最后一维的 len=3 的数组做 mask.
   Returns:
   """
   if not mask_dim < 0:
@@ -39,3 +42,25 @@ def get_gradient(model):
     keys.append(grad)
     add_value4dict(gradients, *keys)
   return gradients
+
+
+def get_parameter_values(model):
+  """
+
+  Args:
+    model:
+
+  Returns:
+
+  """
+  parameter_values = {}
+  for name, parameter in model.named_parameters():
+    keys = name.split(".")
+    data = parameter.data
+    keys.append(data)
+    add_value4dict(parameter_values, *keys)
+  return parameter_values
+
+def gelu(tensor):
+  cdf = 0.5 *(1.0 + torch.erf(tensor/math.sqrt(2.0)))
+  return tensor * cdf
