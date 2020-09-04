032ffe79bf4422549502f4e3f60a4ecaee347019
LL <li.lin@huairuo.ai>
2020-09-04 15:06:16 +0800

    add

--- my_py_toolkit/data_visulization/tensorboard.py
+++ my_py_toolkit/data_visulization/tensorboard.py
@@ -5,9 +5,11 @@
 # cython: language_level=3
 #
 
+from my_py_toolkit.torch.utils import get_gradient, get_parameter_values, process_optimizer_info
+
 def visual_tensorboard(log_dir, tag, data, epoch, step):
   """
-
+  使用 tensorboard 可视化数据
   Args:
     log_dir:
     tag:
@@ -27,3 +29,78 @@ def visual_tensorboard(log_dir, tag, data, epoch, step):
       writer = SummaryWriter(f"{log_dir}/{name}")
       writer.add_histogram(f"{epoch}_{tag}", value, step)
       writer.close()
+
+def is_last_layer(value):
+  if not isinstance(value, dict):
+    return True
+  for k,v in value.items():
+    if isinstance(v, dict):
+      return False
+  return True
+
+def transfer_multi_layer_dict(dict_value):
+  """
+  将多层 dict 转换为 1 层 dict.
+  Args:
+    dict_value:
+
+  Returns:
+
+  """
+  result = {}
+  for key, value in dict_value.items():
+    if not isinstance(value, dict):
+      result[key] = value
+      continue
+
+
+    if not is_last_layer(value):
+      value = transfer_multi_layer_dict(value)
+
+    for sub_key, sub_value in value.items():
+      result[f"{key}.{sub_key}"] = sub_value
+
+  return result
+
+
+def visual_data(model, epoch, step, loss=None, optimizer=None, exact_match_total=None,
+                f1_total=None, exact_match=None, f1=None, label="train", visual_gradient=False,
+                visual_gradient_dir="", visual_parameter=False,
+                visual_parameter_dir=None, visual_loss=False,
+                visual_loss_dir="", visual_optimizer=False,
+                visual_optimizer_dir="", visual_valid_result=False,
+                visual_valid_result_dir=None):
+  """
+  可视化训练信息。
+  Args:
+    model:
+    loss:
+    epoch:
+    step:
+
+  Returns:
+
+  """
+  if visual_gradient:
+    gradient = get_gradient(model)
+    gradient = transfer_multi_layer_dict(gradient)
+    visual_tensorboard(visual_gradient_dir, f"{label}_gradient",
+                       gradient, epoch, step)
+  if visual_parameter:
+    parameter_values = get_parameter_values(model)
+    parameter_values = transfer_multi_layer_dict(parameter_values)
+    visual_tensorboard(visual_parameter_dir, f"{label}_parameter_values",
+                       parameter_values, epoch, step)
+  if visual_loss:
+    visual_tensorboard(visual_loss_dir, f"{label}_loss", {"loss": [loss.item()]},
+                       epoch, step)
+  if visual_optimizer:
+    visual_tensorboard(visual_optimizer_dir, f"{label}_optimizer",
+                       process_optimizer_info(optimizer), epoch, step)
+  if visual_valid_result:
+    visual_tensorboard(visual_valid_result_dir, f"{label}_valid", {
+      "exact_match_total": [exact_match_total],
+      "exact_match": [exact_match],
+      "f1_total": [f1_total],
+      "f1": [f1]
+    }, epoch, step)
---
+++ my_py_toolkit/torch/utils.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Huairuo.ai.
+# Author: Lin Li (li.lin@huairuo.ai)
+#
+# cython: language_level=3
+#
+import copy
+import numpy as np
+import os
+import torch
+from my_py_toolkit.file.file_toolkit import make_path_legal
+
+def add_value4dict(value_dict, *args):
+  args_len = len(args)
+  if args_len == 2:
+    value_dict[args[0]] = args[1]
+    return value_dict
+  elif args_len > 2:
+    value_dict[args[0]] = add_value4dict(value_dict.get(args[0], {}), *args[1:])
+    return value_dict
+  else:
+    raise Exception(f"Args length must longer than 2, now ars is :{args}")
+
+
+def get_gradient(model):
+  """
+  获取模型的梯度。
+  Args:
+    model:
+
+  Returns:
+
+  """
+  gradients = {}
+  for name, parameter in model.named_parameters():
+    keys = name.split(".")
+    grad = parameter.grad
+    keys.append(grad)
+    add_value4dict(gradients, *keys)
+  return gradients
+
+
+def get_parameter_values(model):
+  """
+  获取模型的参数。
+  Args:
+    model:
+
+  Returns:
+
+  """
+  parameter_values = {}
+  for name, parameter in model.named_parameters():
+    keys = name.split(".")
+    data = parameter.data
+    keys.append(data)
+    add_value4dict(parameter_values, *keys)
+  return parameter_values
+
+def process_optimizer_info(optimizer):
+  """获取优化器的信息"""
+  val = copy.deepcopy(optimizer.defaults)
+  result = {}
+  for k,v in val.items():
+    if not isinstance(v, list):
+      v = [v]
+    result[k] = np.asarray(v)
+  return result
+
+
+def save_model(model, model_save_dir, optmizer=None, is_only_save_params=True,
+               epoch=0, steps=0):
+  """
+
+  Args:
+    model:
+    optmizer:
+    model_save_dir:
+    is_only_save_params:
+    epoch:
+    steps:
+
+  Returns:
+
+  """
+  model_path = os.path.join(model_save_dir, f"model_epoch{epoch}_step{str(steps)}.pkl")
+  optimizer_path = os.path.join(model_save_dir, f"optimizer_epoch{epoch}_step{str(steps)}.pkl")
+  make_path_legal(model_path)
+  make_path_legal(optimizer_path)
+  if not is_only_save_params:
+    torch.save(model, model_path)
+    if optmizer:
+      torch.save(optmizer, optimizer_path)
+  else:
+    torch.save(model.state_dict(), model_path)
+    if optmizer:
+      torch.save(optmizer.state_dict(), optimizer_path)
+
+def load_model(model_class, config):
+  """
+  load model
+  Args:
+    model_class:
+    config:
+
+  Returns:
+
+  """
+  model = model_class(config)
+  if config.is_continue_train:
+    model_path = os.path.join(config.model_save_dir,
+                              f"model_epoch{config.continue_epoch}_step{config.continue_checkpoint}.pkl")
+    if config.is_only_save_params:
+      model.load_state_dict(torch.load(model_path))
+    else:
+      model = torch.load(model_path, map_location=config.device)
+  model.to(config.device)
+
+
+def get_model_trainabel_param(model):
+  params = filter(lambda param: param.requires_grad, model.parameters())
+  return params
+
+def get_adam_optimizer(model, config):
+  """
+  Gets optimizer.
+  Returns:
+
+  """
+  params = get_model_trainabel_param(model)
+  if not config.is_continue:
+    return torch.optim.Adam(lr=config.learning_rate, betas=(config.beta1, config.beta2),
+                         eps=1e-8, weight_decay=3e-7, params=params)
+  else:
+    config.logger.info(f"Continue train, epoch:{config.continue_epoch}, continue_checkpoint: {config.continue_checkpoint}")
+    optimizer_path = os.path.join(config.model_dir,
+                              f"optimizer_epoch{config.continue_epoch}_step{config.continue_checkpoint}.pkl")
+    if not config.is_only_save_params:
+      optimizer = torch.load(optimizer_path, map_location=config.device)
+    else:
+      optimizer = torch.optim.Adam(lr=config.learning_rate, betas=(config.beta1, config.beta2),
+                         eps=1e-8, weight_decay=3e-7, params=params)
+      optimizer.load_state_dict(torch.load(optimizer_path))
+    return optimizer
