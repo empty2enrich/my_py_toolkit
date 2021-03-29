da33a28f3ef8316af676a64dbbba1007393029f7
LL <li.lin@huairuo.ai>
2021-03-29 14:31:19 +0800

    encrtpt, decrypt

--- my_py_toolkit/decorator/decorator.py
+++ my_py_toolkit/decorator/decorator.py
@@ -5,7 +5,13 @@
 # cython: language_level=3
 #
 
+import signal
 import time
+from threading import Timer
+
+#################################################################################
+################################ 方法运行时间记录 ################################
+#################################################################################
 
 def fn_timer(logger=None):
   """
@@ -50,3 +56,93 @@ def fn_timer(logger=None):
     return running_time_record
 
   return decorator
+
+
+#################################################################################
+################################ 设置方法的超时时间 ################################
+##################################################################################
+
+"""
+直接设置方法的超时时间
+
+方式一：使用 eventlet (注：针对子进程无法跳出)
+import eventlet
+eventlet.monkey_patch()
+with eventlet.Timeout(2,True):
+  time.sleep(3)
+  
+针对子进程无法跳出：如，一直卡在了 os 模块，不能跳出方法
+import time
+import eventlet
+import os
+eventlet.monkey_patch()   #必须加这条代码
+with eventlet.Timeout(20,False):   #设置超时时间为20秒
+   print '这条语句正常执行'
+   cmd1 = 'binwalk -B {0}'.format(filename)
+   info1_lines = os.popen(cmd1).readlines()
+   cmd2 = 'file {0}'.format(filename)
+   info2_lines = os.popen(cmd2).readlines()
+   print '没有跳过这条输出'
+print '跳过了输出
+
+方式二：func_timeout 模块的@func_set_timeout()
+"""
+
+class TimeoutError(Exception):
+  pass
+
+def set_timeout(num):
+  """
+  signal 只能在 Linux 下使用。
+  Args:
+    num:
+
+  Returns:
+
+  """
+  def wrap(func):
+    def handle(signum,
+               frame):  # 收到信号 SIGALRM 后的回调函数，第一个参数是信号的数字，第二个参数是the interrupted stack frame.
+      print(frame)
+      raise TimeoutError
+
+    def to_do(*args, **kwargs):
+      signal.signal(signal.SIGALRM, handle)  # 设置信号和回调函数
+      signal.alarm(num)  # 设置 num 秒的闹钟
+      print('start alarm signal.')
+      r = func(*args, **kwargs)
+      print('close alarm signal.')
+      signal.alarm(0)  # 关闭闹钟
+      return r
+
+    return to_do
+
+  return wrap
+
+
+
+def time_limit_with_timer(interval):
+  """
+  使用  Time 设置超时时间。
+  注：虽然超时会抛出异常，但不影响方法执行， Timer 是新开的线程几时并抛错，但不能让正在执行的方法退出来。
+  Args:
+    interval:
+
+  Returns:
+
+  """
+  def wraps(func):
+    def time_out():
+      raise RuntimeError()
+
+    def deco(*args, **kwargs):
+      timer = Timer(interval, time_out)
+      timer.start()
+      res = func(*args, **kwargs)
+      timer.cancel()
+      return res
+
+    return deco
+
+  return wraps
+
---
+++ my_py_toolkit/encryption/encrypt.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Huairuo.ai.
+# Author: Lin Li (li.lin@huairuo.ai)
+#
+# cython: language_level=3
+#
+
+import hashlib
+
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+def sha1prng(seed, bit_size=128):
+  """
+  sha1prng 实现, 结果长度为 bit_size/8, 默认 16。
+  Args:
+    seed(str):
+    bit_size(int):
+
+  Returns:
+
+  """
+  return hashlib.sha1(hashlib.sha1(seed.encode()).digest()).digest()[:bit_size//8]
+
+
+def encrypt_aes_sha1prng_ecb(content, key_seed):
+  """
+  使用 sha1prng、AES 的 ecb mode 加密。
+  Args:
+    content(str): 需要加密的内容。
+    key_seed(str): sha1prng加密生成 key 的 seed。
+
+  Returns:
+    (str): 加密后内容。
+  """
+  key = sha1prng(key_seed)
+  cipher = AES.new(key, AES.MODE_ECB)
+  ct_bytes = cipher.encrypt(pad(content, AES.block_size))
+  return b64encode(ct_bytes).decode("utf-8")
+
+def decrypt_aes_sha1prng_ecb(encrypted_content, key_seed):
+  """
+  使用 sha1prng、AES 的 ecb mode 解密。
+  Args:
+    encrypted_content(str): 加密后的内容。
+    key_seed(str): sha1prng加密生成 key 的 seed。
+  Returns:
+    (str):
+  """
+  key = sha1prng(key_seed)
+  ct = b64decode(encrypted_content)
+  cipher = AES.new(key, AES.MODE_ECB)
+  pt = unpad(cipher.decrypt(ct), AES.block_size)
+  return pt.decode('utf-8')
+
